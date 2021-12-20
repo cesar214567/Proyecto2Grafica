7915d5b3c944f1928e34b2bf578a5ca0e8d7e911
cesar214567 <39888085+cesar214567@users.noreply.github.com>
2021-12-20 10:36:18 -0500

    Update f.h

--- letras/f.h
+++ letras/f.h
@@ -46,17 +46,292 @@ public:
       vertices.push_back(temp.first);
       vertices.push_back(temp.second);
       vertices.push_back(1.f);
-      vertices.push_back(1.f);
-      vertices.push_back(0.f);
-      vertices.push_back(0.f);
     }
-
     indices.push_back({0, {9, 10}});
     indices.push_back({0, {1, 10}});
     indices.push_back({1, {3, 4}});
     indices.push_back({1, {2, 4}});
     indices.push_back({5, {7, 8}});
     indices.push_back({5, {6, 8}});
+  }
+  
+  void e(){
+    coords.push_back({-1.f, 1.f});
+    coords.push_back({-0.6f, 1.f});
+    coords.push_back({0.4f, 1.f});
+    coords.push_back({-0.6f, 0.6f});
+    coords.push_back({0.4, 0.6f});
+    coords.push_back({-0.6f, 0.2f});
+    coords.push_back({0.4f, 0.2f});
+    coords.push_back({-0.6f, -0.2f});
+    coords.push_back({0.4f, -0.2f});
+    coords.push_back({-0.6f, -0.6f});
+    coords.push_back({0.6f, 0.6f});
+    coords.push_back({-1.f, -1.f});
+    coords.push_back({-0.6f, 1.f});
+    coords.push_back({0.4f, -1.f});
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {11, 12}});
+    indices.push_back({0, {1, 12}});
+    indices.push_back({1, {3, 4}});
+    indices.push_back({1, {2, 4}});
+    indices.push_back({5, {6, 8}});
+    indices.push_back({5, {6, 7}});
+    indices.push_back({9, {12, 13}});
+    indices.push_back({9, {10, 13}});
+  }
+  
+  void l(){
+    coords.push_back({-1.f, 1.f});
+    coords.push_back({-0.6f, 1.f});
+    coords.push_back({-1f, -1.f});
+    coords.push_back({-0.6f, -0.6f});
+    coords.push_back({0.4, -0.6f});
+    coords.push_back({-0.6f, -1f});
+    coords.push_back({0.4f, -1f});
+    
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {2, 5}});
+    indices.push_back({0, {1, 5}});
+    indices.push_back({3, {5, 6}});
+    indices.push_back({3, {4, 6}});
+      
+    
+  void I(){
+    coords.push_back({-0.8.f, 1.f});
+    coords.push_back({0.8f, 1.f});
+    coords.push_back({-0.8f, -1.f});
+    coords.push_back({0.8f, -0.6f});
+    coords.push_back({-0.2, -0.6f});
+    coords.push_back({0.2f, -1f});
+    coords.push_back({-0.2f, -1f});
+    coords.push_back({0.2f, 1.f});
+    coords.push_back({-0,8f, -1.f});
+    coords.push_back({0,8f, -0.6f});
+    coords.push_back({-0,8, -0.6f});
+    coords.push_back({0,8f, -1f});
+    
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {2, 3}});
+    indices.push_back({0, {3, 1}});
+    indices.push_back({4, {6, 7}});
+    indices.push_back({4, {5, 7}});
+    indices.push_back({8, {10, 11}});
+    indices.push_back({8, {11, 9}});
+      
+      
+  void Z(){
+    coords.push_back({-0.8.f, 1.f});
+    coords.push_back({0.8f, 1.f});
+    coords.push_back({-0.8f, 0.6f});
+    coords.push_back({0.8f, 0.6f});
+    coords.push_back({0.2, 0.6f});
+    coords.push_back({-0.8f, -0.6f});
+    coords.push_back({-0.2f, -0.6f});
+    coords.push_back({0.8f, -0.6f});
+    coords.push_back({-0,8f, -1f});
+    coords.push_back({0,8f, -1f});
+    
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {1, 2}});
+    indices.push_back({1, {2, 3}});
+    indices.push_back({4, {5, 3}});
+    indices.push_back({3, {5, 6}});
+    indices.push_back({5, {8, 9}});
+    indices.push_back({5, {7, 9}});
+        
+        
+  void C(){
+    coords.push_back({-1.f, 1.f});
+    coords.push_back({0.6f, 1.f});
+    coords.push_back({-0.4f, 0.4f});
+    coords.push_back({0.6f, 0.4f});
+    coords.push_back({-0.4, -0.4f});
+    coords.push_back({0.6f, -0.4f});
+    coords.push_back({-0.1f, -1f});
+    coords.push_back({0.6f, -1f});
+    
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {6, 9}});
+    indices.push_back({0, {8, 4}});
+    indices.push_back({8, {2, 1}});
+    indices.push_back({2, {1, 3}});
+    indices.push_back({4, {5, 4}});
+    indices.push_back({5, {9, 7}});
+    
+    
+  void U(){
+    coords.push_back({-0.8.f, 1.f});
+    coords.push_back({-0.4f, 1.f});
+    coords.push_back({0.4f, 1f});
+    coords.push_back({0.8f, 1f});
+    coords.push_back({-0.8f, -0.4f});
+    coords.push_back({-0.4f, -0.4f});
+    coords.push_back({0.4f, -0.4f});
+    coords.push_back({0.8f, -0.4f});
+    coords.push_back({-0.8f, -1f});
+    coords.push_back({0.8f, -1f});
+    
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {4, 5}});
+    indices.push_back({0, {1, 5}});
+    indices.push_back({2, {6, 7}});
+    indices.push_back({2, {3, 7}});
+    indices.push_back({4, {8, 9}});
+    indices.push_back({4, {9, 7}});
+    
+    
+  void M(){
+    coords.push_back({-0.8.f, 1f});
+    coords.push_back({0.8f, 1f});
+    coords.push_back({-0.8f, 0.6f});
+    coords.push_back({0.8f, 0.6f});
+    coords.push_back({-0.4f, 0.6f});
+    coords.push_back({-0.8f, -1f});
+    coords.push_back({-0.4f, -1f});
+    coords.push_back({-0.2f, 0.6f});
+    coords.push_back({-0.2f, 0.6f});
+    coords.push_back({-0.2f, -0.4f});
+    coords.push_back({0.2f, -0.4f});
+    coords.push_back({0.4f, 0.6f});
+    coords.push_back({0.4f, -1f});
+    coords.push_back({0.8f, -1f});
+    
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {2, 3}});
+    indices.push_back({0, {1, 3}});
+    indices.push_back({2, {5, 6}});
+    indices.push_back({2, {4, 6}});
+    indices.push_back({7, {9, 10});
+    indices.push_back({7, {8, 10}});
+    indices.push_back({11, {12, 13}});
+    indices.push_back({11, {3, 13}});
+                       
+                       
+  void P(){
+    coords.push_back({-0.8.f, 1f});
+    coords.push_back({-0.4f, 1f});
+    coords.push_back({-0.8f, -1f});
+    coords.push_back({-0.4f, -1f});
+    coords.push_back({-0.4f, 0.6f});
+    coords.push_back({0.6f, 1f});
+    coords.push_back({0.6f, 0.6f});
+    coords.push_back({-0.4f, 0.4f});
+    coords.push_back({-0.4f, 0f});
+    coords.push_back({0.6f, 0.4f});
+    coords.push_back({0.6f, 0f});
+    coords.push_back({0.2f, 0.6f});
+    coords.push_back({0.2f, 0.4f});
+    
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {2, 3}});
+    indices.push_back({0, {1, 3}});
+    indices.push_back({1, {4, 6}});
+    indices.push_back({1, {5, 6}});
+    indices.push_back({7, {8, 10});
+    indices.push_back({7, {9, 10}});
+    indices.push_back({11, {12, 9}});
+    indices.push_back({11, {6, 9}});
+                       
+                       
+  void V(){
+    coords.push_back({-0.8f, 0.8f});
+    coords.push_back({-0.4f, 0.8f});
+    coords.push_back({0.4f, 0.8f});
+    coords.push_back({0.8f, 0.8f});
+    coords.push_back({-0.2f, -1f});
+    coords.push_back({0f, -0.4f});
+    coords.push_back({0.2f, -1f});
+    
+
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {1, 4}});
+    indices.push_back({1, {4, 5}});
+    indices.push_back({4, {6, 5}});
+    indices.push_back({2, {5, 6}});
+    indices.push_back({2, {3, 6});
+    
+                       
+  void A(){
+    coords.push_back({-0.2f, 1f});
+    coords.push_back({0.2f, 1f});
+    coords.push_back({-0.8f, -1f});
+    coords.push_back({-0.4f, -1f});
+    coords.push_back({0.4f, -1f});
+    coords.push_back({0.8f, -1f});
+    coords.push_back({0f, 0.4f});
+    coords.push_back({-0.4f, -0.2f});
+    coords.push_back({-0.4f, -0.6f});
+    coords.push_back({0.4f, -0.2f});
+    coords.push_back({0.4f, -0.6f});
+
+    
+    for (auto temp:coords)
+    {
+      vertices.push_back(temp.first);
+      vertices.push_back(temp.second);
+      vertices.push_back(1.f);
+    }
+    indices.push_back({0, {2, 3}});
+    indices.push_back({0, {3, 6}});
+    indices.push_back({7, {8, 10}});
+    indices.push_back({7, {9, 10}});
+    indices.push_back({1, {6, 4});
+    indices.push_back({1, {4, 5});
+                       
   }
 
   void setup(){
